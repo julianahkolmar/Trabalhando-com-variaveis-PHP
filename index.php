dfc799a96d2c1303309c294c927ecb2340a8cab5
julianahkolmar <julianakolmar@gmail.com>
2022-07-07 14:43:20 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edit this template
+-->
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Aula 08</title>
+</head>
+
+<body>
+    <?php
+        // put your code here
+        echo "<h1>Trabalhando com variáveis</h1>";
+        $num1 = 1;
+        $num2 = 2;
+        echo('O resultado da soma é: <span style="color:blue">' . $num1 + $num2 . '</span>');
+        
+        ?>
+</body>
+
+</html>
